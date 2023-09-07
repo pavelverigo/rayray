25294bfa30e7d1ec8ab5882da7ce3606e191ce9a
Pavel Verigo <paul.verigo@gmail.com>
2023-09-07 23:29:50 +0200

    glew

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <SDL2/SDL.h>
+#include <GL/glew.h>
 
 #include <iostream>
 
@@ -7,7 +8,18 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("rayray", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+    SDL_Window* window = SDL_CreateWindow("rayray", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+
+    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+
+    glewExperimental = true;
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to initialize GLEW\n";
+        return -1;
+    }
+
+    std::cout << "GLEW version: " << glewGetString(GLEW_VERSION) << std::endl;
+
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
